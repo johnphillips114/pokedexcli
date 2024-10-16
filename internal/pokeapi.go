01a9c07611a720487c197830d77ddcb6125301d3
John Phillips <johnphillips114@gmail.com>
2024-10-16 09:53:53 -0600

    Add structs for Berries, BerryFirmnesses, BerryFlavors, ContestTypes, and ContestEffects

--- internal/pokeapi.go
+++ internal/pokeapi.go
@@ -14,6 +14,13 @@ type Location struct {
 			URL  string `json:"url"`
 		} `json:"language"`
 	} `json:"names"`
+	GameIndicies []struct {
+		Generation struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"generation"`
+		GameIndex uint `json:"game_index"`
+	} `json:"game_indices"`
 	Areas []struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
@@ -22,38 +29,94 @@ type Location struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"region"`
-	GameIndicies []struct {
-		Generation struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"generation"`
-		GameIndex uint `json:"game_index"`
-	} `json:"game_indices"`
 	Name string `json:"name"`
 	ID   uint   `json:"id"`
 }
 
 type LocationAreas struct {
-	ID                   int    `json:"id"`
-	Name                 string `json:"name"`
-	GameIndex            int    `json:"game_index"`
-	EncounterMethodRates []struct {
-		EncounterMethod struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"encounter_method"`
+	ID                int    `json:"id"`
+	Name              string `json:"name"`
+	GameIndex         int    `json:"game_index"`
+	PokemonEncounters []struct {
 		VersionDetails []struct {
-			Rate    int `json:"rate"`
+			EncounterDetails []struct {
+				Method struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"method"`
+				ConditionValues []any `json:"condition_values"`
+				MinLevel        int   `json:"min_level"`
+				MaxLevel        int   `json:"max_level"`
+				Chance          int   `json:"chance"`
+			} `json:"encounter_details"`
 			Version struct {
 				Name string `json:"name"`
 				URL  string `json:"url"`
 			} `json:"version"`
+			MaxChance int `json:"max_chance"`
 		} `json:"version_details"`
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+	} `json:"pokemon_encounters"`
+	EncounterMethodRates []struct {
+		VersionDetails []struct {
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+			Rate int `json:"rate"`
+		} `json:"version_details"`
+		EncounterMethod struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"encounter_method"`
 	} `json:"encounter_method_rates"`
+	Names []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
 	Location struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"location"`
+}
+
+type Berries struct {
+	Flavors []struct {
+		Potency uint `json:"potency"`
+		Flavor  struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"flavor"`
+	} `json:"flavors"`
+	Firmness struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"firmness"`
+	Item struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"item"`
+	NaturalGiftType struct {
+		Name string `json:"name"`
+		url  string `json:"URL"`
+	} `json:"natural_gift_type"`
+	Name             string `json:"name:`
+	ID               uint   `json:"id"`
+	GrowthTime       uint   `json:"growth_time"`
+	MaxHarvest       uint   `json:"max_harvest"`
+	NaturalGiftPower uint   `json:"natural_gift_power"`
+	Size             uint   `json:"size"`
+	Smoothness       uint   `json:"smoothness"`
+	SoilDryness      uint   `json:"soil_dryness"`
+}
+
+type BerryFirmnesses struct {
 	Names []struct {
 		Name     string `json:"name"`
 		Language struct {
@@ -61,52 +124,94 @@ type LocationAreas struct {
 			URL  string `json:"url"`
 		} `json:"language"`
 	} `json:"names"`
-	PokemonEncounters []struct {
-		Pokemon struct {
+	Berries []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"berries"`
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+}
+
+type BerryFlavors struct {
+	Berries []struct {
+		Berry struct {
 			Name string `json:"name"`
 			URL  string `json:"url"`
-		} `json:"pokemon"`
-		VersionDetails []struct {
-			Version struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version"`
-			MaxChance        int `json:"max_chance"`
-			EncounterDetails []struct {
-				MinLevel        int   `json:"min_level"`
-				MaxLevel        int   `json:"max_level"`
-				ConditionValues []any `json:"condition_values"`
-				Chance          int   `json:"chance"`
-				Method          struct {
-					Name string `json:"name"`
-					URL  string `json:"url"`
-				} `json:"method"`
-			} `json:"encounter_details"`
-		} `json:"version_details"`
-	} `json:"pokemon_encounters"`
+		} `json:"berry"`
+		Potency uint `json:"potency"`
+	} `json:"berries"`
+	Names []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	ContestType struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"contest_type"`
+	Name string `json:"name"`
+	ID   uint   `json:"id"`
+}
+
+type ContestTypes struct {
+	Names []struct {
+		Name     string `json:"name"`
+		Color    string `json:"color"`
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+	} `json:"names"`
+	BerryFlavor struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"berry_flavor"`
+	Name string `json:"name"`
+	ID   uint   `json:"id"`
+}
+
+type ContestEffects struct {
+	FlavorTextEntries []struct {
+		FlavorText string `json:"flavor_text"`
+		Language   struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+	} `json:"flavor_text_entries"`
+	EffectEntries []struct {
+		Effect    string `json:"effect"`
+		Lanaguage struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+	} `json:"effect_entries"`
+	ID     uint `json:"id"`
+	Appeal uint `json:"appeal"`
+	Jam    uint `json:"jam"`
 }
 
 func GetLocations(ID int) ([]LocationAreas, error) {
 	locationAreas := make([]LocationAreas, 20)
-	for i := 0; i < 20; i++ {
-		endpoint := "https://pokeapi.co/api/v2/location-area/" + string(ID+i)
-		res, err := http.Get(endpoint)
-		if err != nil {
-			return nil, err
-		}
-		defer res.Body.Close()
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			return nil, err
-		}
-
-		var location LocationAreas
-		err = json.Unmarshal(body, &location)
-		if err != nil {
-			return nil, err
-		}
-		locationAreas[i] = location
+	endpoint := "https://pokeapi.co/api/v2/location-area/" + string(ID)
+	res, err := http.Get(endpoint)
+	if err != nil {
+		return nil, err
 	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(body, &locationAreas)
+	if err != nil {
+		return nil, err
+	}
+	//for i := 0; i < 20; i++ {
+	//	var location LocationAreas
+	//	locationAreas[i] = location
+	//}
 
 	return locationAreas, nil
 }
